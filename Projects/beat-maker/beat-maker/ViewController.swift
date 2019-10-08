dc722964201f99259e7be9c47b9fc9bfa610aa89
Beau Walters <bewa8445@colorado.edu>
2019-10-08 17:52:04 -0600

    implemented play on touch before sound has ended

--- Projects/beat-maker/beat-maker/ViewController.swift
+++ Projects/beat-maker/beat-maker/ViewController.swift
@@ -5,6 +5,10 @@
 //  Created by Beau Walters on 9/29/19.
 //  Copyright © 2019 Beau Walters. All rights reserved.
 //
+//
+
+//adapted from tutorial:
+//https://codewithchris.com/avaudioplayer-tutorial/
 
 import UIKit
 import AVFoundation
@@ -15,11 +19,13 @@ class ViewController: UIViewController {
     var audioPlayer2 = AVAudioPlayer()
     var audioPlayer3 = AVAudioPlayer()
     var audioPlayer4 = AVAudioPlayer()
+    
 
     @IBOutlet weak var drumButton: UIButton!
     @IBOutlet weak var drumButton2: UIButton!
     @IBOutlet weak var drumButton3: UIButton!
     @IBOutlet weak var drumButton4: UIButton!
+    
     
     override func viewDidLoad() {
         
@@ -29,7 +35,7 @@ class ViewController: UIViewController {
         let sound4 = Bundle.main.path(forResource: "highhat", ofType: "wav")
         
         do {
-             audioPlayer1 = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound1!))
+            audioPlayer1 = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound1!))
             audioPlayer2 = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound2!))
             audioPlayer3 = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound3!))
             audioPlayer4 = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound4!))
@@ -40,8 +46,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
     @IBAction func drumDown(_ sender: UIButton) {
-        audioPlayer1.play()
+        if (audioPlayer1.isPlaying) {
+            audioPlayer1.stop()
+            audioPlayer1.currentTime = 0.0
+            audioPlayer1.play()
+        } else {
+            audioPlayer1.play()
+        }
         drumButton.backgroundColor = UIColor .red
     }
     @IBAction func drumReleased(_ sender: UIButton) {
@@ -49,14 +62,26 @@ class ViewController: UIViewController {
         drumButton.backgroundColor = UIColor .blue
     }
     @IBAction func kickDown(_ sender: UIButton) {
-        audioPlayer2.play()
+         if (audioPlayer2.isPlaying) {
+            audioPlayer2.stop()
+            audioPlayer2.currentTime = 0.0
+            audioPlayer2.play()
+         } else {
+            audioPlayer2.play()
+        }
         drumButton2.backgroundColor = UIColor .yellow
     }
     @IBAction func kickReleased(_ sender: UIButton) {
        drumButton2.backgroundColor = UIColor .gray
     }
     @IBAction func cowDown(_ sender: UIButton) {
-        audioPlayer3.play()
+        if (audioPlayer3.isPlaying) {
+            audioPlayer3.stop()
+            audioPlayer3.currentTime = 0.0
+            audioPlayer3.play()
+        } else {
+            audioPlayer3.play()
+        }
         drumButton3.backgroundColor = UIColor .purple
     }
     @IBAction func cowReleased(_ sender: UIButton) {
@@ -64,7 +89,13 @@ class ViewController: UIViewController {
     }
    
     @IBAction func hatDown(_ sender: UIButton) {
-        audioPlayer4.play()
+        if (audioPlayer4.isPlaying) {
+            audioPlayer4.stop()
+            audioPlayer4.currentTime = 0.0
+            audioPlayer4.play()
+        } else {
+            audioPlayer4.play()
+        }
         drumButton4.backgroundColor = UIColor .magenta
     }
     @IBAction func hatReleased(_ sender: UIButton) {
