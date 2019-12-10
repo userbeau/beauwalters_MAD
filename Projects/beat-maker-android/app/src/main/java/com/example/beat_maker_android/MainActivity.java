b78c084993d8149d774d69260d292dcd5c2ef71f
Beau Walters <bewa8445@colorado.edu>
2019-12-09 18:43:55 -0700

    beatmaker layout updates

--- Projects/beat-maker-android/app/src/main/java/com/example/beat_maker_android/MainActivity.java
+++ Projects/beat-maker-android/app/src/main/java/com/example/beat_maker_android/MainActivity.java
@@ -14,31 +14,50 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1;
-    Button button2;
-    Button button3;
-    Button button4;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private MediaPlayer mp1;
+    private MediaPlayer mp2;
+    private MediaPlayer mp3;
+    private MediaPlayer mp4;
+    private MediaPlayer mp5;
+    private MediaPlayer mp6;
+    private MediaPlayer mp7;
+    private MediaPlayer mp8;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        button1 = (Button)findViewById(R.id.eight);
-        button2 = (Button)findViewById(R.id.cowbell);
-        button3 = (Button)findViewById(R.id.highhat);
-        button4 = (Button)findViewById(R.id.kick);
-        final MediaPlayer mp1 = MediaPlayer.create(this, R.raw.eight);
-        final MediaPlayer mp2 = MediaPlayer.create(this, R.raw.cowbell);
-        final MediaPlayer mp3 = MediaPlayer.create(this, R.raw.highhat);
-        final MediaPlayer mp4 = MediaPlayer.create(this, R.raw.kick);
-
-
+        button1 = findViewById(R.id.eight);
+        button2 = findViewById(R.id.cowbell);
+        button3 = findViewById(R.id.highhat);
+        button4 = findViewById(R.id.kick);
+        button5 = findViewById(R.id.piano1);
+        button6 = findViewById(R.id.piano2);
+        button7 = findViewById(R.id.piano3);
+        button8 = findViewById(R.id.piano4);
+        mp1 = MediaPlayer.create(this, R.raw.eight);
+        mp2 = MediaPlayer.create(this, R.raw.cowbell);
+        mp3 = MediaPlayer.create(this, R.raw.highhat);
+        mp4 = MediaPlayer.create(this, R.raw.kick);
+        mp5 = MediaPlayer.create(this, R.raw.piano1);
+        mp6 = MediaPlayer.create(this, R.raw.piano2);
+        mp7 = MediaPlayer.create(this, R.raw.piano3);
+        mp8 = MediaPlayer.create(this, R.raw.piano4);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,8 +85,72 @@ public class MainActivity extends AppCompatActivity {
                 mp4.start();
             }
         });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp5.start();
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp6.start();
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp7.start();
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp8.start();
+            }
+        });
+
+
+        button5.setVisibility(View.GONE);
+        button6.setVisibility(View.GONE);
+        button7.setVisibility(View.GONE);
+        button8.setVisibility(View.GONE);
         }
 
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.drums:
+                if (checked)
+                button5.setVisibility(View.GONE);
+                button6.setVisibility(View.GONE);
+                button7.setVisibility(View.GONE);
+                button8.setVisibility(View.GONE);
+                button1.setVisibility(View.VISIBLE);
+                button2.setVisibility(View.VISIBLE);
+                button3.setVisibility(View.VISIBLE);
+                button4.setVisibility(View.VISIBLE);
+                break;
+            case R.id.piano:
+                if (checked)
+
+                button1.setVisibility(View.GONE);
+                button2.setVisibility(View.GONE);
+                button3.setVisibility(View.GONE);
+                button4.setVisibility(View.GONE);
+                button5.setVisibility(View.VISIBLE);
+                button6.setVisibility(View.VISIBLE);
+                button7.setVisibility(View.VISIBLE);
+                button8.setVisibility(View.VISIBLE);
+
+                break;
+        }
     }
 
+    }
 
